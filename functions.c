45955dde57d29229f795b05e8455952ce4948f26
Brice Duke <bricewduke@gmail.com>
2021-04-09 22:58:41 -0500

    Functions!

---
+++ functions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int addNumbers(int num1, int num2) {
+    return num1 + num2;
+}
+
+int main()
+{
+    // Functions
+
+    // either declare before main function or add a header at the top
+
+    int sum = addNumbers(4, 20);
+
+    return 0;
+}
