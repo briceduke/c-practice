5425b8f916ea25effda5195f9f6b4d4aae6f621b
Brice Duke <bricewduke@gmail.com>
2021-04-09 22:56:58 -0500

    Arrays!

---
+++ arrays.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Arrays
+
+    // Can have multi-dimensional arrays
+
+    int luckyNumbers[] = { 4, 8, 15, 16, 23, 42 }; // [] is array definition, but values are in {}
+
+    luckyNumbers[0] = 90;
+
+    return 0;
+}
